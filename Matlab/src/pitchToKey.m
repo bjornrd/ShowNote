28851675bf907f994d49b5ed19aebd3ecb43d52d
Bjørn Rudi Dahl <bjorn.rudi.dahl@biimultrasound.com>
2021-02-05 15:16:44 +0100

    doc

--- Matlab/src/pitchToKey.m
+++ Matlab/src/pitchToKey.m
@@ -6,6 +6,10 @@ if pitch == 0
     key = 'None';
     
 else
+    % Create name:value pair in a map
+    % name: input frequency
+    % value: output key
+    
     values = {'C0', 'C#0', 'D0', 'D#0', 'E0', 'F0', 'F#0', 'G0', 'G#0', 'A0', 'A#0', 'B0', ...
               'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1', ...
               'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', ...
@@ -16,12 +20,14 @@ else
               'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7', ...
               'C8', 'C#8', 'D8', 'D#8', 'E8', 'F8', 'F#8', 'G8', 'G#8', 'A8', 'A#8', 'B8'};
 
-    vals = -57:50;
-    frequencies = A4_tuning_val*(2^(1/12)).^vals;
-
+    % Calculate pitch for the different piano keys
+    n = -57:50;
+    frequencies = A4_tuning_val*(2^(1/12)).^n; 
+    
+    
     M = containers.Map(frequencies, values);
 
-    [~, mindist] = min(abs(frequencies - pitch)); 
+    [~, mindist] = min(abs(frequencies - pitch));  % Find closest key in the map: Not a very good solution?
 
     key = M(frequencies(mindist));
 end
